28af364efbe8dd3ebc4ab9cf977d8f55971b54d4
aramide ramadan <aramramadan7@gmail.com>
2024-11-06 12:32:23 +0100

    added favicon and error reload button

--- src/components/ErrorComp.tsx
+++ src/components/ErrorComp.tsx
@@ -1,13 +1,18 @@
 function ErrorComp({ error }: { error: string }) {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-6 bg-gray-400 rounded-md">
       <div>
         <p className="text-3xl font-semibold">Oops!.</p>
         <p>An error occured: {error}</p>
       </div>
       <div>
         <p>But don't fret!.</p>
-        <button>Click to Try again</button>
+        <button
+          className="bg-gray-500 rounded-md font-semibold"
+          onClick={() => window.location.reload()}
+        >
+          Click to Try again
+        </button>
       </div>
     </div>
   );
